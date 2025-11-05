f5c2f583c1581061be8c25aeee2ef00069cbb6dc
Abdul Rehman Wakeel <abdulrehman786268@@gmail.com>
2025-11-05 18:41:27 +0500

    Initial commit — Contacts with laravel breeze

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -16,7 +16,7 @@
 <table class="table table-bordered table-striped">
     <thead>
         <tr>
-            <th>#</th><th>Name</th><th>Email</th><th>Message</th><th>Actions</th>
+            <th>ID</th><th>Name</th><th>Email</th><th>Message</th><th>Actions</th>
         </tr>
     </thead>
     <tbody>
